e003ce7b4d2b5fe2c89a510a9773abe6926376b5
iamivet <121934923+iamivet@users.noreply.github.com>
2026-03-31 09:53:44 +0300

    Add a search engine script

--- BookNest/wwwroot/js/site.js
+++ BookNest/wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.getElementById('searchInput');
 
-// Write your JavaScript code.
+    
+    if (searchInput) {
+        searchInput.addEventListener('keyup', function () {
+            let filter = this.value.toLowerCase();
+            let cards = document.querySelectorAll('.book-card');
+
+            cards.forEach(card => {
+               
+                let title = card.querySelector('.book-title').innerText.toLowerCase();
+                let author = card.querySelector('.book-author').innerText.toLowerCase();
+
+              
+                if (title.includes(filter) || author.includes(filter)) {
+                    card.style.display = "";
+                } else {
+                    card.style.display = "none";
+                }
+            });
+        });
+    }
+});
