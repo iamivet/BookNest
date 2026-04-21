42af0c8c3efb92fc947abf260d4ad69304bf970e
iamivet <121934923+iamivet@users.noreply.github.com>
2026-04-21 09:52:40 +0300

    Add js in author's detail page

---
+++ BookNest/wwwroot/js/authorDetails.js
@@ -0,0 +1,23 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const bioText = document.getElementById("bioText");
+    const readMoreBtn = document.getElementById("readMoreBtn");
+
+    if (bioText && readMoreBtn) {
+        // Проверяваме дали текстът е по-дълъг от позволените 6 реда
+        if (bioText.scrollHeight > bioText.clientHeight) {
+            readMoreBtn.style.display = "inline-block";
+        } else {
+            readMoreBtn.style.display = "none"; // Крием бутона, ако биографията е кратка
+        }
+
+        readMoreBtn.addEventListener("click", function () {
+            bioText.classList.toggle("expanded");
+
+            if (bioText.classList.contains("expanded")) {
+                readMoreBtn.innerText = "Скрий текста ▲";
+            } else {
+                readMoreBtn.innerText = "Прочети повече ▼";
+            }
+        });
+    }
+});
