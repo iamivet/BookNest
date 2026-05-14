e0cea68855658a680534eed060e41a426f9bc29b
iamivet <121934923+iamivet@users.noreply.github.com>
2026-05-14 22:57:56 +0300

    Restrict Create, Edit, and Delete actions to Admin role only

--- BookNest/Controllers/BooksController.cs
+++ BookNest/Controllers/BooksController.cs
@@ -3,10 +3,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BookNest.Controllers
 {
@@ -19,15 +15,24 @@ namespace BookNest.Controllers
             _context = context;
         }
 
-        // GET: Books
-        public async Task<IActionResult> Index()
+        public IActionResult Index(string type)
         {
-            var applicationDbContext = _context.Books
-                .Include(b => b.Author)
+            var books = _context.Books.Include(b => b.Author)
                 .Include(b => b.Category)
-                .Include(b => b.Publisher);
+                .Include(b => b.Publisher).AsQueryable();
 
-            return View(await applicationDbContext.ToListAsync());
+            if (type == "BG")
+            {
+                books = books
+                .Where(b => b.Author.Name.EndsWith(" BG"));
+            }
+            else if (type == "Foreign")
+            {
+                     books = books
+                     .Where(b => !b.Author.Name.EndsWith(" BG"));
+            }
+
+            return View(books.ToList());
         }
 
         // GET: Books/Details/5
@@ -52,9 +57,19 @@ namespace BookNest.Controllers
         }
 
         // GET: Books/Create
+        [Authorize(Roles = "Admin")]
         public IActionResult Create()
         {
-            ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "Name");
+
+            var authorsList = _context.Authors.Select(a => new
+            {
+                Id = a.Id,
+                Name = a.Name.EndsWith(" BG")
+                      ? a.Name.Substring(0, a.Name.Length - 3)
+                      : a.Name
+            }).ToList();
+
+            ViewData["AuthorId"] = new SelectList(authorsList, "Id", "Name");
             ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
             ViewData["PublisherId"] = new SelectList(_context.Publishers, "Id", "Name");
             return View();
@@ -63,6 +78,7 @@ namespace BookNest.Controllers
         // POST: Books/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Admin")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Title,YearOfPublication,BookSummary,CoverImage,Price,Quantity,CatalogNumber,AuthorId,PublisherId,CategoryId")] Book book)
@@ -81,6 +97,7 @@ namespace BookNest.Controllers
         }
 
         // GET: Books/Edit/5
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -102,6 +119,7 @@ namespace BookNest.Controllers
         // POST: Books/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Admin")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Title,YearOfPublication,BookSummary,CoverImage,Price,Quantity,AuthorId,PublisherId,CategoryId,RegisterOn")] Book book)
@@ -139,28 +157,8 @@ namespace BookNest.Controllers
             return View(book);
         }
 
-        // GET: Books/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var book = await _context.Books
-                .Include(b => b.Author)
-                .Include(b => b.Category)
-                .Include(b => b.Publisher)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (book == null)
-            {
-                return NotFound();
-            }
-
-            return View(book);
-        }
-
         // POST: Books/Delete/5
+        [Authorize(Roles = "Admin")]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
